d98cec890939cf6f627cc00b972720d760dfa669
YADONG ZHANG <YaD@yadongs-mbp.home>
2016-09-30 18:29:52 +1000

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,6 +12,11 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+
   def create
     @group = Group.new(group_params)
     @group.save
